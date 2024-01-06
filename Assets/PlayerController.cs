e182a053be3cf223366a40e159e44c532bb5f857
LemonShaped <nbshalloe@gmail.com>
2024-01-06 01:22:04 +0000

    oh no i did A LOT without committing...

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,39 +6,74 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public InputAction moveAction; // this has an X and a Y component, but we only use the Y axis if we are flying.
-    public InputAction jumpAction;
-    public Vector2 move;
     public Rigidbody2D rb;
+    public InputAction moveAction;
+    public InputAction jumpAction;
 
-    public float walkSpeed; // walk speed in m/s
-    public float jumpVelocity;
+    public LayerMask groundLayers;
+
+    public Vector2 movement;
+
     public bool doHover;
+    public float walkSpeed; // walking speed in m/s
+    public float jumpVelocity;
+
+    public float jumpHeight;
+    public float jumpTime;
 
     private void OnEnable()
     {
         moveAction.Enable();
-        jumpAction.Enable();
-        jumpAction.performed += OnJump; // we are using a callback so that we dont have to constantly check if whether is pressed.
-    }
 
-    private void Update()
-    {
-        move = moveAction.ReadValue<Vector2>();
+        if (jumpHeight > 0) {
+            jumpVelocity = 2 * jumpHeight / jumpTime; //Mathf.Sqrt(2 * -Physics2D.gravity.y * jumpHeight); //without jumpTime
+            rb.gravityScale = (-jumpVelocity / jumpTime) / Physics2D.gravity.y;
+        }
     }
+    /* s=s u=? v=0 a=-g t=
+       v^2 = u^2 + 2as
+       u = sqrt(-2as)
+       jumpVelocity = sqrt(2*9.8*jumpHeight)
+    */
 
+
+    // s=s  u=?  v=0  a=?  t=t
+    // s = (v+u)/2 * t
+    // u = 2s/t - 0
+    // jumpVelocity = 2*jumpHeight/jumpTime
+    //
+    // v = u + at
+    // a=(v-u)/t
+    // a= -u/t
+    // gravity = -jumpVelocity/jumpTime
     private void FixedUpdate()
     {
-        if (doHover)
-            rb.velocity = move * walkSpeed;
-        else
-            rb.velocityX = Mathf.Ceil(move.x) * walkSpeed; // we ceil it to 1 because move.x is 1.4 if we are holding both sideways and jump
+        movement = moveAction.ReadValue<Vector2>();
+
+        if (doHover) {
+            rb.gravityScale = 0.2f;
+            rb.drag = 5f;
+
+            if (movement.x != 0)
+                rb.velocityX = movement.x * walkSpeed;
+
+            if (movement.y != 0)
+                rb.velocityY = movement.y * walkSpeed;
+
+        }
+        else {
+            rb.gravityScale = 1;
+            rb.drag = 0;
+
+            rb.velocityX = movement.x * walkSpeed;
+
+            // jump
+            if (movement.y > 0 && rb.IsTouchingLayers(groundLayers)) {
+                rb.velocityY = jumpVelocity;
+            }
+
+        }
+
     }
 
-    private void OnJump(InputAction.CallbackContext context)
-    {
-        if (!doHover)
-            rb.velocityY = jumpVelocity;
-    }
 }
