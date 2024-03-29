1595dbc2e9372a5f6bdb8a94ef8c7b9e6bffff5a
LemonShaped <nbshalloe@gmail.com>
2024-03-29 14:22:54 +0000

    Added size logic, made SetCurrentSize method to change values only when size changes; reordered stuff, renamed health to size. (28/03/24)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,58 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
-[Serializable]
-public class MovementMode
-{
-    [HideInInspector] public string name;
-
-    [SerializeField, Tooltip("Max height of jump")]
-    private float _jumpHeight;
-
-    [SerializeField, Tooltip("Time until peak of jump")]
-    private float _jumpDuration;
-
-    public float jumpVelocity;
-
-    [Tooltip("Multiplier applied to gravity")]
-    public float gravityScale;
-
-    [Tooltip("Air resistance")]
-    public float drag;
-
-    [Tooltip("Movement speed in blocks/second")]
-    public float speed;
-
-    public void SetupJump() {
-        if (_jumpHeight != 0 && _jumpDuration != 0) {
-            jumpVelocity = 2 * _jumpHeight / _jumpDuration;
-            gravityScale = (jumpVelocity / _jumpDuration) / -Physics2D.gravity.y;
-        }
-    }
-
-}
 
 public class PlayerController : MonoBehaviour
 {
 
     public InputAction moveAction;
     public InputAction jumpAction;
-    public Vector2 movementInput;
 
     public Rigidbody2D rb;
 
-    public LayerMask groundLayers;
-    public Tilemap groundTilemap;
     public GroundCheck groundCheck;
 
-    [Range(0,5)]
-    public int health = 5;
+    public Tilemap groundTilemap;
+    public LayerMask groundLayers;
 
     public enum ModesEnum {
         Water, Water_Underground, Ice, Cloud
@@ -61,20 +26,31 @@ public class PlayerController : MonoBehaviour
     [NonReorderable]
     public MovementMode[] modes = new MovementMode[4];
 
-    public ModesEnum currentMode;
-    public MovementMode CurrentMode {
+    [NonReorderable]
+    public PlayerSize[] sizes = new PlayerSize[5];
+
+    public ModesEnum currentMode = ModesEnum.Water;
+
+    [Range(0, 4)]
+    public int currentSize;
+
+    public MovementMode ModeData {
         get => modes[(int)currentMode];
+    }
+    public PlayerSize SizeData {
+        get => sizes[currentSize];
     }
     public Vector3Int PlayerTile {
         get => Vector3Int.FloorToInt(transform.position);
     }
 
+
+    private float speed; // calculated based on size and movement mode
+    private float jumpVelocity; //  ''
     private void OnEnable()
     {
         moveAction.Enable();
         jumpAction.Enable();
-
-        OnValidate();
     }
     private void OnDisable()
     {
@@ -90,17 +66,14 @@ public class PlayerController : MonoBehaviour
         }
 
         foreach (MovementMode mode in modes) {
-            //mode.name = Enum.GetName(typeof(ModesEnum), i);
-            mode.SetupJump();
+            mode.name = Enum.GetName(typeof(ModesEnum), i);
         }
     }
 
     private void FixedUpdate()
     {
-        movementInput = moveAction.ReadValue<Vector2>();
 
-        rb.gravityScale = CurrentMode.gravityScale;
-        rb.drag = CurrentMode.drag;
+        Vector2 movementInput = moveAction.ReadValue<Vector2>();
 
         if (movementInput.x < 0)
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
@@ -108,23 +81,23 @@ public class PlayerController : MonoBehaviour
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
 
 
-        if (currentMode == ModesEnum.Cloud || currentMode == ModesEnum.Water_Underground) {
+        if (ModeData.doesJump) {
 
-            if (movementInput.x != 0) // we want to control the speed directly but we dont want to stop instantly, when flying.
-                rb.velocityX = movementInput.x * CurrentMode.speed;
+            rb.velocityX = movementInput.x * speed;
 
-            if (movementInput.y != 0)
-                rb.velocityY = movementInput.y * CurrentMode.speed;
+            // jump
+            if (jumpAction.IsPressed() && IsOnGround()) {
+                rb.velocityY = jumpVelocity;
+            }
 
         }
         else {
 
-            rb.velocityX = movementInput.x * CurrentMode.speed;
+            if (movementInput.x != 0) // we want to control the speed directly but we dont want to stop instantly, when flying.
+                rb.velocityX = movementInput.x * speed;
 
-            // jump
-            if (jumpAction.IsPressed() && IsOnGround()) {
-                rb.velocityY = CurrentMode.jumpVelocity;
-            }
+            if (movementInput.y != 0)
+                rb.velocityY = movementInput.y * speed;
 
         }
 
@@ -140,38 +113,84 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void Hurt() => AddHealth(-1);
 
-    private void AddHealth(int amount) {
-        health += amount;
-        if (health < 0) {
-            health = 0;
+    private bool IsOnGround() {
+        return rb.IsTouchingLayers(groundLayers) && groundCheck.CheckGround(groundLayers);
+    }
+
+    private void Hurt() => AddHealth(-1);
+    private bool AddHealth(int amount) {
+        SetCurrentSize(currentSize + amount);
+        if (currentSize < 0) {
+            SetCurrentSize(0);
             Debug.Log("Dead");//// go to death screen
         }
-        else if (health > 5) {
-            health = 5;
+        else if (currentSize > 4) {
+            SetCurrentSize(4);
         }
     }
+
+
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.name == "Spike") {
             Hurt();
         }
     }
-
-    private bool IsTilePorous(Tilemap tilemap, Vector3Int pos)
+    private bool IsPorous(Vector3Int pos, Tilemap tilemap)
     {
         TileData tileData = default;
         tilemap.GetTile(pos).GetTileData(pos, tilemap, ref tileData);
 
-        return tileData.gameObject.layer == LayerMask.NameToLayer("GroundPorous");
+        return tileData.gameObject != null && tileData.gameObject.layer == LayerMask.NameToLayer("GroundPorous");
         
-    }
 
-    private bool IsOnGround()
+    public void SetCurrentSize(int size)
     {
-        return rb.IsTouchingLayers(groundLayers) && groundCheck.CheckGround(groundLayers);
-    }
+        currentSize = size;
 
+        speed = ModeData.speed * SizeData.speedMultiplier;
+        jumpVelocity = 0;
+
+        if (ModeData.doesJump) {
+            float jumpHeight = ModeData._jumpHeight * SizeData._jumpHeightMultiplier;
+            float jumpDuration = ModeData._jumpDuration * SizeData._jumpDurationMultiplier;
+
+            jumpVelocity = 2 * jumpHeight / jumpDuration;
+            rb.gravityScale = (jumpVelocity / jumpDuration) / -Physics2D.gravity.y;
+        }
+        else {
+            rb.gravityScale = ModeData.gravityScale * SizeData.gravityScaleMultiplier;
+            rb.drag = ModeData.drag * SizeData.dragMultiplier;
+            
+        }
+    }
 
 }
+
+
+[Serializable]
+public class MovementMode
+{
+    [HideInInspector]
+    public string name;
+
+    [Tooltip("Movement speed in blocks/second")]
+    public float speed;
+
+    [Tooltip("Air resistance")]
+    public float drag;
+
+    [Tooltip("Multiplier applied to gravity. If doesJump, this is ignored and is calculated automatically")]
+    public float gravityScale;
+
+    public bool doesJump = false;
+
+    [Tooltip("Max height of jump")]
+    public float _jumpHeight;
+    [Tooltip("Time until peak of jump")]
+    public float _jumpDuration;
+
+}
+
+
