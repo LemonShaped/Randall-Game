89b47e358f51c20e7c1677b71802e1de5a27ac77
LemonShaped <nbshalloe@gmail.com>
2026-02-11 03:39:08 +0000

    Switch to Rider, cleaned up old editor files. updated gitignore

--- GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: SuppressMessage("Style", "IDE0074:Use compound assignment", Justification = "<Pending>", Scope = "member", Target = "~P:LiquidCharacter.CurrentMode")]
-[assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<Pending>", Scope = "member", Target = "~M:HelperMethods.Yellow(UnityEngine.Color)~UnityEngine.Color")]
