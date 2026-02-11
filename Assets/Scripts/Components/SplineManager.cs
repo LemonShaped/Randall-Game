493a78df32fac34286e998629ea562755f7e826f
LemonShaped <nbshalloe@gmail.com>
2026-02-11 03:40:55 +0000

    Splines..

---
+++ Assets/Scripts/Components/SplineManager.cs
@@ -0,0 +1,90 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+using UnityEngine.U2D;
+
+public class SplineManager : MonoBehaviour
+{
+    public SpriteShapeController[] spriteShapeControllers;
+
+    public SplineContainer splineContainer;
+    public int splineIndex;
+
+    public UnityEngine.Splines.Spline spline => splineContainer.Splines[splineIndex];
+
+    [SerializeField] bool recreate;
+
+    public new PolygonCollider2D collider;
+
+    [SerializeField] new bool print;
+
+    void Recreate()
+    {
+        foreach (var c in spriteShapeControllers) {
+            UnityEngine.U2D.Spline spriteSpline = c.spline;
+
+            spriteSpline.Clear();
+            spriteSpline.isOpenEnded = false;
+
+            int ssPointCount = spriteSpline.GetPointCount();
+            for (int index = 0; index < Mathf.Max(ssPointCount, spline.Count); index++) {
+
+                if (index >= ssPointCount)
+                    spriteSpline.InsertPointAt(index, spline[index].Position);
+
+                else if (index >= spline.Count) {
+                    spriteSpline.RemovePointAt(index);
+                    continue;
+                }
+
+                spriteSpline.SetCorner(index, true);
+                spriteSpline.SetHeight(index, 1f);
+                spriteSpline.SetPosition(index, spline[index].Position);
+                spriteSpline.SetSpriteIndex(index, 0);
+
+                if (spline.GetTangentMode(index) == TangentMode.Linear) {
+                    spriteSpline.SetTangentMode(index, ShapeTangentMode.Linear);
+                    spriteSpline.SetLeftTangent(index, Vector3.zero);
+                    spriteSpline.SetRightTangent(index, Vector3.zero);
+                }
+                else {
+                    spriteSpline.SetTangentMode(index, ShapeTangentMode.Broken);
+                    spriteSpline.SetLeftTangent(index, math.rotate(spline[index].Rotation, spline[index].TangentIn));
+                    spriteSpline.SetRightTangent(index, math.rotate(spline[index].Rotation, spline[index].TangentOut));
+                }
+
+            }
+        }
+        collider = spriteShapeControllers[0].polygonCollider;
+        spriteShapeControllers[0].BakeCollider();
+    }
+
+    void Print()
+    {
+        var log = "";
+
+        var count = spriteShapeControllers[0].spline.GetPointCount();
+        log += "Point count " + count;
+
+        for (int index = 0; index < count; index++) {
+            log += "\n\nindex: " + index;
+            log += "\nPosition " + spriteShapeControllers[0].spline.GetPosition(index);
+            log += "\nLeftTangent" + spriteShapeControllers[0].spline.GetLeftTangent(index);
+            log += "\nRightTangent" + spriteShapeControllers[0].spline.GetRightTangent(index);
+        }
+
+        Debug.Log(log);
+    }
+
+    private void OnValidate()
+    {
+        if (recreate) {
+            recreate = false;
+            Recreate();
+        }
+        if (print) {
+            print = false;
+            Print();
+        }
+    }
+}
--- Assets/Scripts/Editor/CsprojPostprocessor.cs
+++ Assets/Scripts/Editor/CsprojPostprocessor.cs
@@ -8,8 +8,7 @@ public class CsprojPostprocessor : AssetPostprocessor
 {
     public static string OnGeneratedCSProject(string path, string content)
     {
-        try
-        {
+        try {
 
             if (!path.EndsWith("Assembly-CSharp.csproj"))
                 return content;
@@ -31,29 +30,32 @@ public class CsprojPostprocessor : AssetPostprocessor
 </Project>
 ";
         }
-        catch (Exception)
-        {
+        catch (Exception) {
             return content;
         }
     }
 
     public static string OnGeneratedSlnSolution(string path, string content)
-    {/*
-        Match projectsMatch = Regex.Match(content, @"^# Visual Studio \d\d(?:\r\n?|\n)+(^Project\("".*^EndProject$(?:\r\n?|\n))^Global$", RegexOptions.Multiline | RegexOptions.Singleline);
+    {
+        /*
+        Debug.Log("generating sln");
+        Debug.Log(content);
 
-        if (!projectsMatch.Success)
+        Match projectsMatch = Regex.Match(content, @"^# Visual Studio \d+(?:\r\n?|\n)+(^Project\("".*)(^Project\(""[^""]*""\) = ""[^""]*"", ""Assembly-CSharp\.csproj"", ""[^""]+""(?:\r\n?|\n)^EndProject(?:\r\n?|\n))(.*^EndProject$(?:\r\n?|\n))^Global$",
+            RegexOptions.Multiline | RegexOptions.Singleline
+        );
+
+        if (!projectsMatch.Success) {
+            Debug.Log("sadface");
             return content;
-        string projects = projectsMatch.Result("$1");
+        }
 
+        string projects = projectsMatch.Groups[1].Value + projectsMatch.Groups[2].Value + projectsMatch.Groups[3].Value;
+        string assemblyCsharp = projectsMatch.Groups[2].Value;
 
-        Match assemblyCsharpMatch = Regex.Match(projects, @"(^Project\(""[^""]*""\) = ""[^""]*"", ""Assembly-CSharp\.csproj"", ""[^""]+""(?:\r\n?|\n)^EndProject(?:\r\n?|\n))", RegexOptions.Multiline | RegexOptions.Singleline);
+        Debug.Log("game assembly: " + assemblyCsharp);
 
-        if (!assemblyCsharpMatch.Success)
-            return content;
-        string assemblyCsharp = assemblyCsharpMatch.Result("$1");
-
-
-        return content.Replace(projects, assemblyCsharp + content.Replace(assemblyCsharp, ""));
+        return Regex.Replace(content, Regex.Escape(projects), assemblyCsharp + projectsMatch.Groups[1].Value + projectsMatch.Groups[3].Value);
         */
         return content;
     }
